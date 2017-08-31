f7feabdc9895b0dc0855554284f56e4b5b407f04
elminsterii <elminsterii2@gmail.com>
2017-09-01 00:29:15 +0800

    [LittleBee] add drawer and define few definitions

--- app/src/main/java/elminsterii/littlebee/MainActivity.java
+++ app/src/main/java/elminsterii/littlebee/MainActivity.java
@@ -1,23 +1,37 @@
 package elminsterii.littlebee;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar myToolbar;
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] mPlanetTitles;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+        initToolBar();
+        initDrawerLayout();
 //
 //        // Inflate your custom layout
 //        final ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(
@@ -30,6 +44,55 @@ public class MainActivity extends AppCompatActivity {
 //        actionBar.setDisplayShowTitleEnabled(false);
 //        actionBar.setDisplayShowCustomEnabled(true);
 //        actionBar.setCustomView(actionBarLayout);
+    }
+
+    private void initToolBar() {
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    private void initDrawerLayout() {
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mPlanetTitles = getResources().getStringArray(R.array.drawer_list);
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+                GravityCompat.START);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                myToolbar, R.string.drawer_open,
+                R.string.drawer_close) {
+
+            /**
+             * Called when a drawer has settled in a completely closed state.
+             */
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu();
+            }
+
+            /**
+             * Called when a drawer has settled in a completely open state.
+             */
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerList.setAdapter(new ArrayAdapter<>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                mDrawerList.setItemChecked(position, true);
+                setTitle(mPlanetTitles[position]);
+                mDrawerLayout.closeDrawer(mDrawerList);
+            }
+        });
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
     }
 
     @Override
@@ -65,8 +128,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onPrepareOptionsMenu(menu);
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
 
         switch (item.getItemId()) {
             case R.id.menu_setting:
@@ -79,6 +158,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
         }
 
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 }
